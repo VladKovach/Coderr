04f825af768516ef9cc27fc0e3c836bf9c9b0590
Vlad Kovach <vladkovach00@gmail.com>
2026-02-11 13:51:23 +0100

    auth_app test for serializers

--- auth_app/tests/test_serializers.py
+++ auth_app/tests/test_serializers.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from auth_app.api.serializers import RegistrationSerializer
+from auth_app.api.serializers import LoginSerializer, RegistrationSerializer
 
 
 class RegistrationSerializerTest(TestCase):
@@ -12,8 +12,8 @@ class RegistrationSerializerTest(TestCase):
         data = {
             "username": "",
             "email": "wrong",
-            "password": "abc123",
-            "repeated_password": "wrong",
+            "password": "examplePassword",
+            "repeated_password": "examplePassword",
             "type": "wrong",
         }
 
@@ -22,5 +22,37 @@ class RegistrationSerializerTest(TestCase):
 
         self.assertIn("username", serializer.errors)
         self.assertIn("email", serializer.errors)
+        self.assertIn("type", serializer.errors)
+
+    def test_passwords_not_match(self):
+        """
+        Ensure validating.
+        """
+        data = {
+            "username": "exampleUsername",
+            "email": "example@mail.de",
+            "password": "examplePassword1111",
+            "repeated_password": "examplePassword2222",
+            "type": "customer",
+        }
+
+        serializer = RegistrationSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+
         self.assertIn("repeated_password", serializer.errors)
+
+
+class LoginSerializerTest(TestCase):
+
+    def test_invalid_fields(self):
+        """
+        Ensure validating.
+        """
+        data = {"username": "", "password": ""}
+
+        serializer = LoginSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+
+        self.assertIn("username", serializer.errors)
+        self.assertIn("email", serializer.errors)
         self.assertIn("type", serializer.errors)
