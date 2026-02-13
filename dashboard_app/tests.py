566e243883bb3d9ee08ab3e105d558c09f05dc6c
Vlad Kovach <vladkovach00@gmail.com>
2026-02-13 14:29:28 +0100

    simple test for dashboard_app

---
+++ dashboard_app/tests.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class BaseInfoTests(APITestCase):
+    def test_get_base_info_ok(self):
+        """
+        Ensure we can get .
+        """
+        url = reverse("baseinfo")
+        response = self.client.get(url)
+        print("response = ", response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertIn("review_count", data)
+        self.assertIn("average_rating", data)
+        self.assertIn("business_profile_count", data)
+        self.assertIn("offer_count", data)
